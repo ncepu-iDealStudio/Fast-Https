a073734f11b55609e6870a47f3cc4f63bfb5a5a7
pzclinux <927068267@qq.com>
2023-07-09 17:55:08 +0800

    add gitignore

--- modules/server/build/CMakeFiles/fasthttps.dir/flags.make
+++ modules/server/build/CMakeFiles/fasthttps.dir/flags.make
@@ -6,5 +6,5 @@ C_FLAGS =
 
 C_DEFINES = 
 
-C_INCLUDES = -I/home/pzc/Project/fast-https/modules/modules/server 
+C_INCLUDES = -I/home/pzc/Project/fast-https/modules/server 
 
--- modules/server/build/cmake_install.cmake
+++ modules/server/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/pzc/Project/fast-https/modules/modules/server
+# Install script for directory: /home/pzc/Project/fast-https/modules/server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/pzc/Project/fast-https/modules/modules/server/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/pzc/Project/fast-https/modules/server/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
