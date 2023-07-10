3c249e57cdb7f65b4a593d13bf6f6383c6221783
jackiex <xjg@ncepu.edu.cn>
2023-07-10 13:52:47 +0800

    add structure folder and files

--- cmd/commands.go
+++ cmd/commands.go
@@ -1,1 +1,38 @@
 package cmd
+
+import (
+	"fast-https/utils"
+	"fast-https/utils/errHelper"
+	"fast-https/utils/message"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "project_layout",
+	Short: "fast-https",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Start doing things.
+		message.Println("Start Server.....")
+		utils.GetWaitGroup().Add(1)
+
+		// check something on here
+
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	errHelper.ErrExit(rootCmd.Execute())
+}
+
+func init() {
+	//rootCmd.PersistentFlags().StringP("Port", "P", "8000", "配置文件名(注意-C为大写)")
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	//port, err := rootCmd.Flags().GetString("Port")
+	//errHelper.ErrExit(err)
+}
---
+++ init/init.go
@@ -0,0 +1,60 @@
+package init
+
+import (
+	"fast-https/utils"
+	"fast-https/utils/loggers"
+	"fast-https/utils/message"
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"sync"
+)
+
+func Init() *sync.WaitGroup {
+	// 消息初始化
+	waitGroup := MessageInit()
+
+	// 配置读取初始化
+	err := ViperInit()
+	if err != nil {
+		message.PrintErr(err)
+		message.Exit()
+	}
+
+	// logger object initial
+	LoggerInit(viper.GetString("log.type"))
+
+	return waitGroup
+}
+
+func MessageInit() *sync.WaitGroup {
+	waitGroup := utils.GetWaitGroup()
+	waitGroup.Add(1)
+	go message.InitMsg()
+	return waitGroup
+}
+
+// ViperInit viper object init
+func ViperInit() (err error) {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./configs") // 添加搜索路径
+	viper.SetConfigType("yaml")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Fatal error config file: ", err)
+		return
+	}
+	viper.WatchConfig()
+
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file:", e.Name, "Op: ", e.Op)
+	})
+
+	return
+}
+
+// LoggerInit Log object init
+func LoggerInit(logType string) {
+	loggers.InitLogger(logType)
+}
