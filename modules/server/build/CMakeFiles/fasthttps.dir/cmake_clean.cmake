b16a48697d38829e1f62542d5d868756d0739f4a
pzclinux <927068267@qq.com>
2023-07-10 13:24:24 +0800

    marge

---
+++ modules/server/build/CMakeFiles/fasthttps.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fasthttps.dir/dm_events.c.o"
+  "CMakeFiles/fasthttps.dir/dm_master.c.o"
+  "CMakeFiles/fasthttps.dir/dm_server.c.o"
+  "CMakeFiles/fasthttps.dir/dm_socket.c.o"
+  "CMakeFiles/fasthttps.dir/dm_threading_pool.c.o"
+  "CMakeFiles/fasthttps.dir/dm_timer.c.o"
+  "CMakeFiles/fasthttps.dir/main.c.o"
+  "fasthttps"
+  "fasthttps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fasthttps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
