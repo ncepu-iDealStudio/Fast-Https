51d017a4df008f1b31c460ca67b73264accb8988
pzclinux <927068267@qq.com>
2023-07-13 23:15:42 +0800

    nothing

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-<<<<<<< HEAD
-=======
 import (
-	"fast-https/config"
+	_ "fast-https/config"
+	"fast-https/modules/cache"
+	"fast-https/modules/core/server"
 )
 
->>>>>>> 4e5114c7748c7764fada2154ed949d688d737b99
 func main() {
-	//config.Process()
-	// server.Daemon(0, 1)
-	// fmt.Println(config.G_config.HttpServer[0].Path)
-<<<<<<< HEAD
-	//server.Run()
-	//httpparse.Process_HttpParse()
-=======
-	// server.Run()
->>>>>>> 4e5114c7748c7764fada2154ed949d688d737b99
+
+	cache.LoadAllStatic()
+
+	server.Run()
+
 }
