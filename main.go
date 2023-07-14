ffc0c64ab118c36e3dc98b198dcfef024d1f8501
wanfengsix <2368498274@qq.com>
2023-07-14 16:04:12 +0800

    no message

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
-<<<<<<< HEAD
-=======
-import (
-	"fast-https/config"
-)
-
->>>>>>> 4e5114c7748c7764fada2154ed949d688d737b99
 func main() {
 	//config.Process()
 	// server.Daemon(0, 1)
 	// fmt.Println(config.G_config.HttpServer[0].Path)
-<<<<<<< HEAD
 	//server.Run()
 	//httpparse.Process_HttpParse()
-=======
-	// server.Run()
->>>>>>> 4e5114c7748c7764fada2154ed949d688d737b99
 }
