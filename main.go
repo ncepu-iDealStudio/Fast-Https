3ebe7a1e167fa9873e107b1e8a5cf1fc60358f0b
jackiex <xjg@ncepu.edu.cn>
2023-07-14 16:16:40 +0800

    到处代码冲突

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-<<<<<<< HEAD
-func main() {
-	//config.Process()
-	// server.Daemon(0, 1)
-	// fmt.Println(config.G_config.HttpServer[0].Path)
-	//server.Run()
-	//httpparse.Process_HttpParse()
-=======
 import (
 	"fast-https/cmd"
 	_ "fast-https/config"
@@ -18,5 +10,4 @@ func main() {
 	cache.LoadAllStatic()
 	cmd.Execute()
 
->>>>>>> 7d181f62054dbea9b80706977d341143f684f31b
 }
