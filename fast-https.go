7800c54357e173f38866a8c8c09c3e2f7c988cd2
jackiex <xjg@ncepu.edu.cn>
2023-07-17 18:16:14 +0800

    update default config file

--- fast-https.go
+++ fast-https.go
@@ -4,15 +4,9 @@ import (
 	"fast-https/cmd"
 	_ "fast-https/config"
 	"fast-https/modules/cache"
-	"fast-https/service"
 )
 
 func main() {
 	cache.LoadAllStatic()
 	cmd.Execute()
-
-	service.TestService("127.0.0.1:5000")
-	// server.Run()
-
-	// cache.TestCsGzip()
 }
