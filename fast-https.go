bedc79188e8a4fed683f3f8f81a44c118a8a4801
serendipiy8 <2085616045@qq.com>
2023-07-18 16:27:59 +0800

    cmd

--- fast-https.go
+++ fast-https.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fast-https/cmd"
 	_ "fast-https/config"
+	"fast-https/modules/cache"
 )
 
 func main() {
-	//cache.LoadAllStatic()
-	cmd.Execute()
+	rootcmd := cmd.NewRootCmd()
+	cache.LoadAllStatic()
+	rootcmd.Execute()
 }
