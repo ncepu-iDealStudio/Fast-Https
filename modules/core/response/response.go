99ddcac7713eaa6f12c47e5ef37febcd34ba2368
pzclinux <927068267@qq.com>
2023-07-21 11:30:18 +0800

    add response.go

---
+++ modules/core/response/response.go
@@ -0,0 +1,52 @@
+package response
+
+import (
+	"fmt"
+)
+
+type Response struct {
+	firstLine string
+	headers   map[string]string
+	body      []byte
+}
+
+func Response_init() *Response {
+	return &Response{
+		headers: make(map[string]string),
+	}
+}
+
+func (r *Response) Set_first_line(statusCode int, statusText string) {
+	r.firstLine = fmt.Sprintf("HTTP/1.1 %d %s", statusCode, statusText)
+}
+
+func (r *Response) Set_header(key, value string) {
+	r.headers[key] = value
+}
+
+func (r *Response) Set_body(body []byte) {
+	r.body = body
+}
+
+func (r *Response) Generate_response() []byte {
+	var res []byte
+	response := r.firstLine + HTTP_SPLIT
+	for key, value := range r.headers {
+		response += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+	res = append(res, []byte(response)...)
+	res = append(res, r.body...)
+
+	return res
+}
+
+func Test() {
+	response := Response_init()
+
+	response.Set_first_line(200, "OK")
+	response.Set_header("Content-Type", "text/html")
+	response.Set_body([]byte("<h1>Hello, World!</h1>"))
+
+	Response := response.Generate_response()
+	fmt.Println(Response)
+}
--- modules/core/response/response_default.go
+++ modules/core/response/response_default.go
@@ -1,4 +1,4 @@
-package events
+package response
 
 const (
 	HTTP_SPLIT = "\r\n"
@@ -10,3 +10,5 @@ const (
 	HTTP_NOTFOUND     = "HTTP/1.1 404 NOTFOUND"
 	HTTP_SERVER_ERROR = "HTTP/1.1 500 SERVERERROR"
 )
+
+var Default_not_found []byte = []byte(HTTP_NOTFOUND + HTTP_SPLIT + HTTP_SPLIT + "[event_static:65]: Can't find this file")
