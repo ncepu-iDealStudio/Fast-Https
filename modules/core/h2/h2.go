b46db0d0029a9801ebc725305e529e61e3bf825b
pzclinux <927068267@qq.com>
2023-09-02 14:47:00 +0800

    try to add h2

--- modules/core/h2/h2.go
+++ modules/core/h2/h2.go
@@ -1,10 +1,82 @@
 package h2
 
 import (
+	"bufio"
+	"crypto/rand"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/net/http2"
 )
+
+// ioBuf := bufio.NewReader(each_event.Conn)
+// req, err := http.ReadRequest(ioBuf)
+// if err != nil {
+// 	log.Println(err)
+// 	break
+// }
+// bodyByte, _ := io.ReadAll(req.Body)
+// log.Println("recv: ", string(bodyByte))
+// buf := bytes.NewBuffer(nil)
+// buf.WriteString("HTTP/2 200 OK\r\n")
+// buf.WriteString("Content-Length: " + strconv.Itoa(len(bodyByte)) + "\r\n")
+// // buf.WriteString("Connection: keep-alive\r\n")
+// buf.WriteString("\r\n")
+// buf.Write(bodyByte)
+// buf.WriteTo(each_event.Conn)
+
+func Test() {
+	certs := []tls.Certificate{}
+	crt, err := tls.LoadX509KeyPair("./config/cert/localhost.pem", "./config/cert/localhost-key.pem")
+	if err != nil {
+		fmt.Println("load err")
+	}
+	certs = append(certs, crt)
+	tlsConfig := &tls.Config{}
+	tlsConfig.Certificates = certs
+	tlsConfig.Time = time.Now
+	tlsConfig.Rand = rand.Reader
+	tlsConfig.NextProtos = append(tlsConfig.NextProtos, "h1")
+	tlsConfig.NextProtos = append(tlsConfig.NextProtos, "h2")
+	tlsConfig.NextProtos = append(tlsConfig.NextProtos, "h2c")
+
+	lis, err := tls.Listen("tcp", "0.0.0.0:5555", tlsConfig)
+	if err != nil {
+		fmt.Println("listen err")
+	}
+
+	for {
+
+		conn, _ := lis.Accept()
+
+		fmt.Println(conn.RemoteAddr().String())
+
+		for {
+			rBuf := bufio.NewReader(conn)
+			frame, err := http2.ReadFrameHeader(rBuf)
+
+			if err != nil {
+				fmt.Println(err)
+				conn.Close()
+				break
+			}
+
+			// Decode the frame
+			switch frame.Header().Type {
+			case http2.FrameHeaders:
+				fmt.Println("StreamID", frame.Header())
+
+			case http2.FrameData:
+				fmt.Println("data", frame.Header())
+			}
+
+		}
+	}
+
+}
 
 func H2() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +92,5 @@ func H2() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
--- modules/core/response/response_default.go
+++ modules/core/response/response_default.go
@@ -11,9 +11,22 @@ const (
 )
 
 const (
+	HTTP_TEST         = "<h1 style='text-align:center;'>This is a test Page</h1>"
 	HTTP_NOTFOUND     = "<h1 style='text-align:center;'>404 Not Found!</h1>"
 	HTTP_SERVER_ERROR = "<h1 style='text-align:center;'>500 Server Error!</h1>"
 )
+
+func Default_test() []byte {
+	res := Response_init()
+	res.Set_first_line(200, "OK")
+	res.Set_header("Server", "Fast-Https")
+	res.Set_header("Date", time.Now().String())
+
+	res.Set_header("Content-Type", "text/html")
+	res.Set_header("Content-Length", strconv.Itoa(len([]byte(HTTP_NOTFOUND))))
+	res.Set_body([]byte(HTTP_NOTFOUND))
+	return res.Generate_response()
+}
 
 func Default_not_found() []byte {
 	res := Response_init()
