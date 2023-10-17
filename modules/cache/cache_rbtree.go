6fa2960e0ee2bcab842ea16c708f1ba6a72d9093
yizhigopher <yizhigopher@foxmail.com>
2023-10-17 16:39:46 +0800

    将红黑树的锁改成粗粒度，为整棵树加读写锁

--- modules/cache/cache_rbtree.go
+++ modules/cache/cache_rbtree.go
@@ -8,13 +8,12 @@ import (
 )
 
 type redblackNode struct {
-	mu        sync.RWMutex // 为每个结点增加一个读写锁
 	cacheNode *CacheNode
 }
 
 type RBtree struct {
 	tree *rbt.Tree
-	mu   sync.Mutex // for delete node
+	mu   sync.RWMutex // for delete node
 }
 
 func NewRBtree() *RBtree {
@@ -27,8 +26,8 @@ func (t *RBtree) AddInRbtree(data *CacheNode) {
 	node := &redblackNode{
 		cacheNode: data,
 	}
-	node.mu.Lock()
-	defer node.mu.Unlock()
+	t.mu.Lock()
+	defer t.mu.Unlock()
 	t.tree.Put(data.Md5, node)
 }
 
@@ -39,13 +38,13 @@ func (t *RBtree) RemoveFromRBtreeByKey(key string) {
 }
 
 func (t *RBtree) GetNodeFromRBtreeByKey(key string) (*CacheNode, bool) {
+	t.mu.RLock()
 	node_i, found := t.tree.Get(key)
 	if !found {
 		return nil, false
 	}
 	node := node_i.(*redblackNode)
-	node.mu.RLock()
-	defer node.mu.RUnlock()
+	t.mu.RUnlock()
 	return node.cacheNode, true
 }
 
@@ -66,9 +65,9 @@ func (t *RBtree) UpdateExpire(key string, expire int) bool {
 		return false
 	}
 	rbtree := value.(*redblackNode)
-	rbtree.mu.Lock()
+	t.mu.Lock()
 	rbtree.cacheNode.Expire = int(time.Now().Add(time.Duration(expire)).Unix())
-	rbtree.mu.Unlock()
+	t.mu.Unlock()
 	return true
 }
 
@@ -82,9 +81,9 @@ func (t *RBtree) UpdatePath(key string, path string, expire int) bool {
 		return false
 	}
 	rbtree := value.(*redblackNode)
-	rbtree.mu.Lock()
+	t.mu.Lock()
 	rbtree.cacheNode.Expire = int(time.Now().Add(time.Duration(expire)).Unix())
 	rbtree.cacheNode.Path = path
-	rbtree.mu.Unlock()
+	t.mu.Unlock()
 	return true
 }
