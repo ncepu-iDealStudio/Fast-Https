0b6012b392f4044e9cba77a3f1414e2fa96827d4
serendipiy8 <2085616045@qq.com>
2023-12-21 13:22:13 +0800

    black_list and slide_win

--- modules/safe/blacklist.go
+++ modules/safe/blacklist.go
@@ -1,1 +1,215 @@
 package safe
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+)
+
+// 用于存储黑名单的结构体
+type Blacklist struct {
+	ipRanges []string
+	mu       sync.RWMutex
+}
+
+// 创建一个新的黑名单
+func NewBlacklist() *Blacklist {
+	return &Blacklist{
+		ipRanges: make([]string, 0),
+	}
+}
+
+// 获取黑名单中的所有 IP 地址范围
+func (b *Blacklist) GetIPRanges() []string {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	return b.ipRanges
+}
+
+// 将 IP 或 IP 段添加到黑名单
+func (b *Blacklist) Add(ipOrRange string) error {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	// 检查重复
+	if b.isDuplicate(ipOrRange) {
+		return fmt.Errorf("重复的 IP 地址或范围: %s", ipOrRange)
+	}
+
+	// 处理逗号分隔的 IP 列表
+	ipList, err := parseIPList(ipOrRange)
+	if err != nil {
+		return err
+	}
+
+	// 将每个 IP 添加到黑名单
+	for _, ip := range ipList {
+		if strings.Contains(ip, "-") {
+			// 处理 IP 段
+			ipRangeList, err := parseIPRange(ip)
+			if err != nil {
+				return err
+			}
+
+			b.ipRanges = append(b.ipRanges, ipRangeList...)
+		} else {
+			// 处理单个 IP
+			parsedIP := net.ParseIP(ip)
+			if parsedIP == nil {
+				return fmt.Errorf("无效的 IP 地址: %s", ip)
+			}
+			b.ipRanges = append(b.ipRanges, parsedIP.String())
+		}
+	}
+
+	return nil
+}
+
+// 从黑名单中移除 IP 或 IP 段
+func (b *Blacklist) Remove(ipOrRange string) error {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	// 检查要移除的 IP 是否存在
+	if !b.isInBlacklist(ipOrRange) {
+		return fmt.Errorf("IP 或 IP 段 %s 不在黑名单中", ipOrRange)
+	}
+
+	if strings.Contains(ipOrRange, "-") {
+		// 处理 IP 段
+		ipList, err := parseIPRange(ipOrRange)
+		if err != nil {
+			return err
+		}
+
+		for _, ip := range ipList {
+			for i, existingIP := range b.ipRanges {
+				if existingIP == ip {
+					// 找到匹配的 IP，移除
+					b.ipRanges = append(b.ipRanges[:i], b.ipRanges[i+1:]...)
+				}
+			}
+		}
+	} else {
+		// 处理单个 IP
+		parsedIP := net.ParseIP(ipOrRange)
+		if parsedIP == nil {
+			return fmt.Errorf("无效的 IP 地址: %s", ipOrRange)
+		}
+
+		for i, existingIP := range b.ipRanges {
+			if existingIP == parsedIP.String() {
+				// 找到匹配的 IP，移除
+				b.ipRanges = append(b.ipRanges[:i], b.ipRanges[i+1:]...)
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("IP 或 IP 段 %s 不在黑名单中", ipOrRange)
+}
+
+// 检查是否存在重复的 IP 地址或范围
+func (b *Blacklist) isDuplicate(ipOrRange string) bool {
+	for _, existingIP := range b.ipRanges {
+		if existingIP == ipOrRange {
+			return true
+		}
+	}
+	return false
+}
+
+// 检查 IP 或 IP 段是否在黑名单中
+func (b *Blacklist) isInBlacklist(ipOrRange string) bool {
+	for _, existingIP := range b.ipRanges {
+		if existingIP == ipOrRange {
+			return true
+		}
+	}
+	return false
+}
+
+// 解析 IP 列表并返回 IP 地址列表
+func parseIPList(ipListStr string) ([]string, error) {
+	ipList := strings.Split(ipListStr, ",")
+
+	var result []string
+	for _, ipStr := range ipList {
+		ipStr = strings.TrimSpace(ipStr)
+		if ipStr != "" {
+			result = append(result, ipStr)
+		}
+	}
+
+	return result, nil
+}
+
+// 解析 IP 段并返回 IP 地址列表
+func parseIPRange(ipRange string) ([]string, error) {
+	parts := strings.Split(ipRange, "-")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("无效的 IP 段格式")
+	}
+
+	startIP := net.ParseIP(strings.TrimSpace(parts[0]))
+	endIP := net.ParseIP(strings.TrimSpace(parts[1]))
+
+	if startIP == nil || endIP == nil {
+		return nil, fmt.Errorf("无效的 IP 地址")
+	}
+
+	var ips []string
+	for ip := startIP; ip.String() != endIP.String(); {
+		ips = append(ips, ip.String())
+		ip = nextIP(ip)
+	}
+	ips = append(ips, endIP.String())
+
+	return ips, nil
+}
+
+// 计算下一个 IP 地址
+func nextIP(ip net.IP) net.IP {
+	next := make(net.IP, len(ip))
+	copy(next, ip)
+
+	for j := len(next) - 1; j >= 0; j-- {
+		next[j]++
+		if next[j] > 0 {
+			break
+		}
+	}
+
+	return next
+}
+
+// func main() {
+// 	// 创建一个新的黑名单
+// 	blacklist := NewBlacklist()
+
+// 	// 向黑名单中添加 IP
+// 	blacklist.Add("127.0.0.2")
+// 	blacklist.Add("192.168.1.2")
+
+// 	// 添加 IP 段
+// 	blacklist.Add("192.168.1.10,192.168.1.15")
+
+// 	// 获取黑名单中的所有 IP 地址范围
+// 	ipRanges := blacklist.GetIPRanges()
+// 	fmt.Println("黑名单中的所有 IP 地址范围:")
+// 	for _, ipRange := range ipRanges {
+// 		fmt.Println(ipRange)
+// 	}
+
+// 	// 从黑名单中移除 IP
+// 	blacklist.Remove("127.0.0.2")
+
+// 	// 再次获取黑名单中的所有 IP 地址范围
+// 	ipRanges = blacklist.GetIPRanges()
+// 	fmt.Println("\n移除 IP 后的所有 IP 地址范围:")
+// 	for _, ipRange := range ipRanges {
+// 		fmt.Println(ipRange)
+// 	}
+// }
--- modules/safe/slide_win.go
+++ modules/safe/slide_win.go
@@ -1,1 +1,35 @@
 package safe
+
+type IP string
+
+var deny []string
+
+type FrontendDemo struct {
+	Idx int
+	IP  [1000]IP
+}
+
+func (fd *FrontendDemo) insert(ip string) {
+	if fd.Idx >= 1000 {
+		fd.Idx = 0
+	}
+	fd.IP[fd.Idx] = IP(ip)
+	fd.Idx++
+}
+
+func (fd *FrontendDemo) check() bool {
+	deny = []string{"192.168.1.1", "192.168.1.2"}
+	return true
+}
+
+// func main() {
+// 	fd := &FrontendDemo{}
+// 	fd.insert("192.168.1.1")
+// 	fd.insert("192.168.1.2")
+// 	go func() {
+// 		for {
+// 			fd.check()
+// 			time.Sleep(time.Second)
+// 		}
+// 	}()
+// }
