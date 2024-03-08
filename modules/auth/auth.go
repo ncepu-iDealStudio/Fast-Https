5b59fb0e646f30a3a19389f2e5faeda855f817a9
wanfengsix <2368498274@qq.com>
2024-03-08 21:19:04 +0800

    auth:debug

--- modules/auth/auth.go
+++ modules/auth/auth.go
@@ -13,6 +13,9 @@ import (
 // 在这里传参进入简单的用户名和密码，实际使用中应该使用更安全的方式存储和验证
 
 func AuthHandler(cfg *listener.ListenCfg, ev *core.Event) bool {
+	if !AuthExists(cfg) {
+		return true
+	}
 	req := ev.RR.Req_
 	basic := req.GetHeader("Authorization")
 	var username string
@@ -38,8 +41,15 @@ func AuthHandler(cfg *listener.ListenCfg, ev *core.Event) bool {
 	res := response.ResponseInit()
 	res.SetFirstLine(401, "Authorization Required")
 	res.SetHeader("www-Authenticate", "Basic realm=\"Access to the staging site\"")
-	rr := ([]byte)("HTTP/1.1 401 Authorization Required\r\nwww-Authenticate: Basic realm=\"Access to the staging site\"\r\n\r\n")
-	ev.WriteDataClose(rr)
 
+	ev.WriteDataClose(res.GenerateResponse())
 	return false
+}
+
+func AuthExists(cfg *listener.ListenCfg) bool {
+	if cfg.Auth.AuthType != "" {
+		return true
+	} else {
+		return false
+	}
 }
