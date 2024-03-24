ff252f12a76e66c34b3d49c5290d3695b64f6d8d
Ajax <927068267@qq.com>
2024-03-24 16:06:42 +0800

    debug websocket

--- modules/websocket/websocket.go
+++ modules/websocket/websocket.go
@@ -6,60 +6,90 @@ import (
 	"fast-https/utils/message"
 	"fmt"
 	"io"
+	"net"
+	"time"
 )
 
 // like a tcp proxy
+// todo: improve buffer
 func WebSocketHandler(ev *core.Event) {
-	fmt.Println("+++++++++++++++++++++++++")
+	// fmt.Println("+++++++++++++++++++++++++")
 
 	proxy, flag := (ev.RR.CircleData).(*proxy.Proxy)
 	if !flag {
 		message.PrintErr("--proxy can not convert circle data to *Proxy")
 	}
 
+	go func() {
+		buffer := make([]byte, 1024)
+		for {
+			ev.Conn.SetDeadline(time.Now().Add(time.Second * 10))
+			n, err := ev.Conn.Read(buffer)
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("eof 1")
+					break
+				} else if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+					// fmt.Println("time out once 1")
+					continue
+				} else {
+					message.PrintWarn("[1]websocket: Proxy read error ", err.Error())
+					proxy.ProxyConn.Close()
+					return
+				}
+			}
+
+			// fmt.Println("read from postman:", buffer[:n])
+
+			_, err = proxy.ProxyConn.Write(buffer[:n])
+			if err != nil {
+				if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+					fmt.Println("websocket server closed ...")
+					ev.Close()
+					return
+				} else {
+					proxy.ProxyConn.Close()
+					message.PrintWarn("websocket: Proxy Write error ", err.Error())
+					break
+				}
+			}
+		}
+	}()
+
 	buffer := make([]byte, 1024)
 	for {
-		n, err := ev.Conn.Read(buffer)
+
+		proxy.ProxyConn.SetDeadline(time.Now().Add(time.Second * 10))
+		n, err := proxy.ProxyConn.Read(buffer)
 		if err != nil {
 			if err == io.EOF {
+				fmt.Println("eof 2")
 				break
+			} else if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+				// fmt.Println("time out once 2")
+				continue
 			} else {
-				message.PrintWarn("[1]websocket: Proxy read error", err.Error())
+				message.PrintWarn("websocket: Proxy read error ", err.Error())
 				proxy.ProxyConn.Close()
 				return
 			}
+
 		}
 
-		if err == io.EOF {
-			continue
-		}
-		fmt.Println(buffer[:n])
-
-		_, err = proxy.ProxyConn.Write(buffer[:n])
-		if err != nil {
-			proxy.ProxyConn.Close()
-			message.PrintWarn("websocket: Proxy Write error", err.Error())
-			break
-		}
-
-		// ___________________________________
-
-		n, err = proxy.ProxyConn.Read(buffer)
-		if err != nil && err != io.EOF {
-			message.PrintWarn("websocket: Proxy read error", err.Error())
-			proxy.ProxyConn.Close()
-			break
-		}
-		if err == io.EOF {
-			continue
-		}
-		fmt.Println(buffer[:n])
+		// fmt.Println("read from websocket server", buffer[:n])
 
 		_, err = ev.Conn.Write(buffer[:n])
 		if err != nil {
-			message.PrintWarn("websocket: Proxy Write error", err.Error())
-			proxy.ProxyConn.Close()
-			break
+
+			if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+				fmt.Println("postman client closed ...")
+				proxy.ProxyConn.Close()
+				return
+			} else {
+				message.PrintWarn("websocket: Proxy Write error ", err.Error())
+				proxy.ProxyConn.Close()
+				break
+			}
 		}
 	}
 
