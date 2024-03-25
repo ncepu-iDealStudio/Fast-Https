5e46a3bb8969df2374a409cd0254e629fb003cf7
JiayueZ <studentzhang2002@163.com>
2024-03-25 11:24:49 +0800

    add requset test

--- modules/core/response/response.go
+++ modules/core/response/response.go
@@ -33,27 +33,27 @@ func (e *ResponError) Error() string {
 // every event will return a Response object
 // except tcp proxy
 type Response struct {
-	firstLine string
-	headers   map[string]string
-	body      []byte
+	FirstLine string
+	Headers   map[string]string
+	Body      []byte
 }
 
 func ResponseInit() *Response {
 	return &Response{
-		headers: make(map[string]string),
+		Headers: make(map[string]string),
 	}
 }
 
 func (r *Response) SetFirstLine(statusCode int, statusText string) {
-	r.firstLine = fmt.Sprintf("HTTP/1.1 %d %s", statusCode, statusText)
+	r.FirstLine = fmt.Sprintf("HTTP/1.1 %d %s", statusCode, statusText)
 }
 
 func (r *Response) SetHeader(key, value string) {
-	r.headers[key] = value
+	r.Headers[key] = value
 }
 
 func (r *Response) SetBody(body []byte) {
-	r.body = body
+	r.Body = body
 }
 
 // Generate a response data (bytes)
@@ -61,13 +61,13 @@ func (r *Response) SetBody(body []byte) {
 // once response contain '\0', it will doesn't work
 func (r *Response) GenerateResponse() []byte {
 	var res []byte
-	response := r.firstLine + HTTP_SPLIT
-	for key, value := range r.headers {
+	response := r.FirstLine + HTTP_SPLIT
+	for key, value := range r.Headers {
 		response += fmt.Sprintf("%s: %s\r\n", key, value)
 	}
 	res = append(res, []byte(response)...)
 	res = append(res, []byte("\r\n")...)
-	res = append(res, r.body...)
+	res = append(res, r.Body...)
 
 	return res
 }
@@ -94,7 +94,7 @@ func (r *Response) HttpResParse(request string) error {
 		parts := strings.SplitN(line, ":", 2)
 		key := strings.TrimSpace(parts[0])
 		value := strings.TrimSpace(parts[1])
-		r.headers[key] = value
+		r.Headers[key] = value
 	}
 
 	return ResponseOk // valid
@@ -102,7 +102,7 @@ func (r *Response) HttpResParse(request string) error {
 
 // get request header
 func (r *Response) GetHeader(key string) string {
-	return r.headers[key]
+	return r.Headers[key]
 }
 
 func Test() {
